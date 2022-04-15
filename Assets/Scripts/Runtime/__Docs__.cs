62bb1bdf17bf14c773d12a78f1d8472167fd9fd3
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-04-15 10:32:16 -0400

    Update __Docs__.cs

--- Assets/Scripts/Runtime/__Docs__.cs
+++ Assets/Scripts/Runtime/__Docs__.cs
@@ -1,5 +1,5 @@
 /// <summary>
-/// Contains components for interfacting the ManiaMap library with Unity.
+/// Contains components for interfacting the %ManiaMap library with %Unity.
 /// </summary>
 namespace MPewsey.ManiaMap.Unity
 {
