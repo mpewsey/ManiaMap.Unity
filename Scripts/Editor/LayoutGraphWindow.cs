ef8768b7206387ac1a6a24ec752f825854c0b8c7
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-04-24 11:57:39 -0400

    Update LayoutGraphWindow.cs

--- Scripts/Editor/LayoutGraphWindow.cs
+++ Scripts/Editor/LayoutGraphWindow.cs
@@ -7,8 +7,9 @@ namespace MPewsey.ManiaMap.Unity.Editor
 {
     public class LayoutGraphWindow : EditorWindow
     {
-        private const float MenuBarHeight = 25;
         private static SerializedObject SerializedObject { get; set; }
+
+        private Uid SelectionId { get; set; }
 
         private static LayoutGraph GetLayoutGraph()
         {
@@ -19,7 +20,7 @@ namespace MPewsey.ManiaMap.Unity.Editor
         {
             SerializedObject = new SerializedObject(graph);
             var window = GetWindow<LayoutGraphWindow>();
-            window.titleContent = new GUIContent("Layout Graph Editor");
+            window.titleContent = new GUIContent("Graph");
             window.minSize = new Vector2(600, 200);
             window.maxSize = new Vector2(1920, 720);
         }
@@ -39,14 +40,62 @@ namespace MPewsey.ManiaMap.Unity.Editor
             }
             
             SerializedObject.Update();
+            SetWindowTitle();
             DrawMenuBar();
+            ProcessMouseEvents();
             SerializedObject.ApplyModifiedProperties();
+        }
+
+        private void SetWindowTitle()
+        {
+            var name = SerializedObject.targetObject.name;
+            titleContent.text = $"Graph [{name}]";
         }
 
         private void DrawMenuBar()
         {
-            GUILayout.BeginArea(new Rect(0, 0, position.width, MenuBarHeight), GUI.skin.box);
-            GUILayout.EndArea();
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            DrawEditMenuButton();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        private void DrawEditMenuButton()
+        {
+            if (GUILayout.Button("Edit", EditorStyles.toolbarDropDown))
+            {
+                var menu = new GenericMenu();
+                menu.AddItem(new GUIContent("Create Node"), false, CreateNode);
+                menu.DropDown(new Rect(0, 8, 0, 16));
+            }
+        }
+
+        private void ProcessMouseEvents()
+        {
+            const int RightMouseButton = 1;
+            var window = new Rect(0, 0, position.width, position.height);
+
+            if (!window.Contains(Event.current.mousePosition))
+                return;
+
+            if (Event.current.button == RightMouseButton)
+                DrawContextMenu();
+        }
+
+        private void DrawContextMenu()
+        {
+            var menu = new GenericMenu();
+            menu.AddItem(new GUIContent("Create Node"), false, CreateNode);
+            menu.AddItem(new GUIContent("Create Edge"), false, () => Debug.Log("Create Edge"));
+            menu.AddSeparator("");
+            menu.AddItem(new GUIContent("Delete Node"), false, () => Debug.Log("Delete Node"));
+            menu.AddItem(new GUIContent("Delete Edge"), false, () => Debug.Log("Delete Edge"));
+            menu.DropDown(new Rect(Event.current.mousePosition, Vector2.zero));
+        }
+
+        private void CreateNode()
+        {
+            GetLayoutGraph().CreateNode();
         }
     }
 }
