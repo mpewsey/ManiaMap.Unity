636daec9ed96789332b8802eef4f6bb8a27e77f7
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-05-20 14:12:43 -0400

    Update room template

--- Scripts/Runtime/Cell.cs
+++ Scripts/Runtime/Cell.cs
@@ -6,27 +6,86 @@ namespace MPewsey.ManiaMap.Unity
     {
         [SerializeField]
         private RoomTemplate _template;
+        /// <summary>
+        /// The parent room template.
+        /// </summary>
         public RoomTemplate Template { get => _template; set => _template = value; }
 
         [SerializeField]
         private Vector2Int _position;
-        public Vector2Int Position { get => _position; set => _position = value; }
-
-        [SerializeField]
-        private Vector2 _cellSize = Vector2.one;
-        public Vector2 CellSize { get => _cellSize; set => _cellSize = Vector2.Max(value, Vector2.one); }
+        /// <summary>
+        /// The index position of the cell in the room template.
+        /// </summary>
+        public Vector2Int Position { get => _position; set => _position = Vector2Int.Max(value, Vector2Int.zero); }
 
         [SerializeField]
         private bool _isEmpty;
+        /// <summary>
+        /// True if the cell is empty.
+        /// </summary>
         public bool IsEmpty { get => _isEmpty; set => _isEmpty = value; }
 
+        /// <summary>
+        /// Initializes the cell properties.
+        /// </summary>
+        /// <param name="template">The parent room template.</param>
+        /// <param name="position">The index position of the cell in the room template.</param>
         public void Init(RoomTemplate template, Vector2Int position)
         {
             name = $"<Cell ({position.x}, {position.y})>";
             Template = template;
             Position = position;
-            CellSize = template.CellSize;
-            transform.localPosition = new Vector2(CellSize.x * position.y, -CellSize.y * position.x);
+            transform.localPosition = Center();
+        }
+
+        private void OnValidate()
+        {
+            Position = Position;
+        }
+
+        private void OnDrawGizmos()
+        {
+            var cellSize = Template.GetPlaneVector(Template.CellSize);
+
+            // Draw fill color.
+            Gizmos.color = IsEmpty ? new Color(0, 0, 0, 0.25f) : new Color(0.5f, 0.5f, 0.5f, 0.25f);
+            Gizmos.DrawCube(transform.position, cellSize);
+            
+            // Draw outline.
+            Gizmos.color = Color.grey;
+            Gizmos.DrawWireCube(transform.position, cellSize);
+
+            // If empty, draw an X through the cell.
+            if (IsEmpty)
+            {
+                var right = Template.GetPlaneVector(Vector2.right);
+                var up = Template.GetPlaneVector(Vector2.up);
+                var from1 = Origin();
+                var to1 = from1 + cellSize;
+                var from2 = from1 + new Vector3(cellSize.x * Mathf.Abs(right.x), cellSize.y * Mathf.Abs(right.y), cellSize.z * Mathf.Abs(right.z));
+                var to2 = from1 + new Vector3(cellSize.x * Mathf.Abs(up.x), cellSize.y * Mathf.Abs(up.y), cellSize.z * Mathf.Abs(up.z));
+                Gizmos.color = Color.grey;
+                Gizmos.DrawLine(from1, to1);
+                Gizmos.DrawLine(from2, to2);
+            }
+        }
+
+        /// <summary>
+        /// Returns the local position for the origin of the cell.
+        /// </summary>
+        public Vector3 Origin()
+        {
+            var cellSize = Template.GetPlaneVector(Template.CellSize);
+            var index = Template.GetPlaneVector(new Vector2(Position.y, Position.x));
+            return new Vector3(cellSize.x * Mathf.Abs(index.x), cellSize.y * Mathf.Abs(index.y), cellSize.z * Mathf.Abs(index.z));
+        }
+
+        /// <summary>
+        /// Returns the local position for the center of the cell.
+        /// </summary>
+        public Vector3 Center()
+        {
+            return Origin() + 0.5f * Template.GetPlaneVector(Template.CellSize);
         }
     }
 }
--- Scripts/Runtime/RoomTemplate.cs
+++ Scripts/Runtime/RoomTemplate.cs
@@ -26,14 +26,17 @@ namespace MPewsey.ManiaMap.Unity
 
         [SerializeField]
         private Transform _cellContainer;
+        /// <summary>
+        /// The cell container.
+        /// </summary>
         public Transform CellContainer { get => _cellContainer; set => _cellContainer = value; }
 
         [SerializeField]
-        private Vector2Int _size = Vector2Int.one;
+        private Plane _cellPlane;
         /// <summary>
-        /// The size of the room template grid.
+        /// The plane in which the cells reside.
         /// </summary>
-        public Vector2Int Size { get => _size; set => _size = Vector2Int.Max(value, Vector2Int.one); }
+        public Plane CellPlane { get => _cellPlane; set => _cellPlane = value; }
 
         [SerializeField]
         private Vector2 _cellSize = Vector2.one;
@@ -43,8 +46,11 @@ namespace MPewsey.ManiaMap.Unity
         public Vector2 CellSize { get => _cellSize; set => _cellSize = Vector2.Max(value, Vector2.one); }
 
         [SerializeField]
-        private List<Cell> _cells = new List<Cell>();
-        public List<Cell> Cells { get => _cells; set => _cells = value; }
+        private Vector2Int _size = Vector2Int.one;
+        /// <summary>
+        /// The size of the room template grid.
+        /// </summary>
+        public Vector2Int Size { get => _size; set => _size = Vector2Int.Max(value, Vector2Int.one); }
 
         private void OnValidate()
         {
@@ -52,21 +58,29 @@ namespace MPewsey.ManiaMap.Unity
             CellSize = CellSize;
         }
 
+        public Vector3 GetPlaneVector(Vector2 vector)
+        {
+            switch (CellPlane)
+            {
+                case Plane.XY:
+                    return new Vector3(vector.x, -vector.y, 0);
+                case Plane.XZ:
+                    return new Vector3(vector.x, 0, -vector.y);
+                default:
+                    throw new System.ArgumentException($"Unhandled cell plane: {CellPlane}.");
+            }
+        }
+
         public Cell GetCell(int row, int column)
         {
             if (!CellIndexExists(row, column))
                 throw new System.IndexOutOfRangeException($"Index out of range: ({row}, {column}).");
-            return Cells[FlatCellIndex(row, column)];
+            return CellContainer.GetChild(row).GetChild(column).GetComponent<Cell>();
         }
 
-        public bool CellIndexExists(int row, int column)
+        private bool CellIndexExists(int row, int column)
         {
             return (uint)row < Size.x && (uint)column < Size.y;
-        }
-
-        private int FlatCellIndex(int row, int column)
-        {
-            return row * Size.y + column;
         }
 
         public void CreateCellContainer()
@@ -77,41 +91,68 @@ namespace MPewsey.ManiaMap.Unity
                 obj.transform.SetParent(transform);
                 CellContainer = obj.transform;
             }
+
+            CellContainer.gameObject.hideFlags = HideFlags.NotEditable;
         }
 
         public void CreateCells()
         {
             CreateCellContainer();
-            Cells.RemoveAll(x => x == null);
+
+            // Destroy extra rows.
+            while (CellContainer.childCount > Size.x)
+            {
+                var row = CellContainer.GetChild(CellContainer.childCount - 1);
+                DestroyImmediate(row.gameObject);
+            }
+
+            // Create new rows.
+            while (CellContainer.childCount < Size.x)
+            {
+                var obj = new GameObject("<New Row>");
+                obj.transform.SetParent(CellContainer);
+            }
+
+            for (int i = 0; i < Size.x; i++)
+            {
+                CreateRow(i);
+            }
+        }
+
+        private void CreateRow(int row)
+        {
+            var container = CellContainer.GetChild(row);
+            container.gameObject.hideFlags = HideFlags.NotEditable;
+            container.name = $"<Row {row}>";
 
             // Destroy extra cells.
-            while (Cells.Count > Size.x * Size.y)
+            while (container.childCount > Size.y)
             {
-                var cell = Cells[Cells.Count - 1];
-                Cells.RemoveAt(Cells.Count - 1);
+                var cell = container.GetChild(container.childCount - 1);
                 DestroyImmediate(cell.gameObject);
             }
 
             // Create new cells.
-            while (Cells.Count < Size.x * Size.y)
+            while (container.childCount < Size.y)
             {
-                var obj = new GameObject();
-                obj.transform.SetParent(CellContainer);
-                var cell = obj.AddComponent<Cell>();
-                Cells.Add(cell);
+                var obj = new GameObject("<New Cell>");
+                obj.transform.SetParent(container);
+                obj.transform.hideFlags = HideFlags.NotEditable;
+                obj.AddComponent<Cell>();
             }
 
-            // Initialize cells
-            int k = 0;
-
-            for (int i = 0; i < Size.x; i++)
+            for (int j = 0; j < Size.y; j++)
             {
-                for (int j = 0; j < Size.y; j++)
-                {
-                    var index = new Vector2Int(i, j);
-                    Cells[k++].Init(this, index);
-                }
+                var index = new Vector2Int(row, j);
+                var cell = container.GetChild(j).GetComponent<Cell>();
+                cell.Init(this, index);
             }
+        }
+
+        public enum Plane
+        {
+            XY,
+            XZ,
         }
     }
 }
