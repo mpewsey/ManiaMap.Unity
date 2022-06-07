f219c43408a1b682e88e1306125eb122a4a49383
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-06-07 10:12:36 -0400

    Update LayoutMap

--- Scripts/Tests/PlayMode/Drawing/TestLayoutMap.cs
+++ Scripts/Tests/PlayMode/Drawing/TestLayoutMap.cs
@@ -24,17 +24,6 @@ namespace MPewsey.ManiaMap.Unity.Drawing.Tests
             Object.DestroyImmediate(Container);
         }
 
-        private GenerationPipeline LoadBigLayoutGenerator()
-        {
-            var path = "Packages/com.mpewsey.maniamap.unity/Prefabs/BigLayoutGenerator.prefab";
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            Assert.IsNotNull(prefab);
-            var obj = Object.Instantiate(prefab, Container.transform);
-            var generator = obj.GetComponent<GenerationPipeline>();
-            Assert.IsNotNull(generator);
-            return generator;
-        }
-
         private LayoutMap LoadLayoutMap()
         {
             var path = "Packages/com.mpewsey.maniamap.unity/Prefabs/LayoutMap.prefab";
@@ -49,30 +38,14 @@ namespace MPewsey.ManiaMap.Unity.Drawing.Tests
         [Test]
         public void TestSaveImages()
         {
-            var random = new RandomSeed(12345);
-            var graph = Samples.GraphLibrary.BigGraph();
-            var templateGroups = Samples.BigLayoutSample.BigLayoutTemplateGroups();
-            var collectableGroups = new CollectableGroups();
-            collectableGroups.Add("Default", new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-
-            var inputs = new Dictionary<string, object>
-            {
-                { "LayoutId", 1 },
-                { "LayoutGraph", graph },
-                { "TemplateGroups", templateGroups },
-                { "CollectableGroups", collectableGroups },
-                { "RandomSeed", random },
-            };
-
-            var pipeline = ManiaMap.GenerationPipeline.CreateDefaultPipeline();
-            var results = pipeline.Generate(inputs);
+            var results = Samples.BigLayoutSample.Generate(12345);
             Assert.IsTrue(results.Success);
             var layout = (Layout)results.Outputs["Layout"];
             Assert.IsNotNull(layout);
             var layoutMap = LoadLayoutMap();
             layoutMap.Init(layout);
             Directory.CreateDirectory("Tests");
-            layoutMap.SaveImages("Tests/BigLayout.png");
+            layoutMap.SaveImages("Tests/BigLayoutMap.png");
         }
     }
 }
