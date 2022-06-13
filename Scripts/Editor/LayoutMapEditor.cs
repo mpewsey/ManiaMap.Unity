6979face13b3b56b6e210cec7b54709fd1552d5e
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-06-13 14:18:24 -0400

    Update LayoutMap (#6)

---
+++ Scripts/Editor/LayoutMapEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MPewsey.ManiaMap.Unity.Drawing.Editor
+{
+    public static class LayoutMapEditor
+    {
+        [MenuItem("GameObject/Mania Map/Layout Map", priority = 20)]
+        [MenuItem("Mania Map/Create Layout Map", priority = 100)]
+        public static void CreateLayoutMap()
+        {
+            var obj = new GameObject("Layout Map");
+            obj.AddComponent<LayoutMap>();
+            obj.transform.SetParent(Selection.activeTransform);
+        }
+    }
+}
