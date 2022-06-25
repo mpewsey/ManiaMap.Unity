db3adda569f675acc1066c3c864ca3c50262b12b
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-06-25 17:30:38 -0400

    Update docs

--- Scripts/Runtime/CollectableGroupsInput.cs
+++ Scripts/Runtime/CollectableGroupsInput.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 
 namespace MPewsey.ManiaMap.Unity
 {
+    /// <summary>
+    /// A generation input for supplying collectable groups to a GenerationPipeline.
+    /// </summary>
     public class CollectableGroupsInput : GenerationInput
     {
         [SerializeField]
         private List<CollectableGroup> _collectableGroups = new List<CollectableGroup>();
+        /// <summary>
+        /// A list of collectable groups.
+        /// </summary>
         public List<CollectableGroup> CollectableGroups { get => _collectableGroups; set => _collectableGroups = value; }
 
+        /// <inheritdoc/>
         public override void AddInput(Dictionary<string, object> input)
         {
             input.Add("CollectableGroups", GetCollectableGroups());
         }
 
+        /// <inheritdoc/>
+        public override string[] OutputNames()
+        {
+            return new string[] { "CollectableGroups" };
+        }
+
+        /// <summary>
+        /// Returns the collectable groups for the input.
+        /// </summary>
         public CollectableGroups GetCollectableGroups()
         {
             var groups = new CollectableGroups();
@@ -24,11 +40,6 @@ namespace MPewsey.ManiaMap.Unity
             }
 
             return groups;
-        }
-
-        public override string[] OutputNames()
-        {
-            return new string[] { "CollectableGroups" };
         }
     }
 }
