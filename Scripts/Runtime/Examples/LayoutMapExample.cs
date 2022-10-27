862589901d1ba2db7eb4bba9de102697843f4d54
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-10-27 14:58:47 -0400

    Update samples

--- Scripts/Runtime/Examples/LayoutMapExample.cs
+++ Scripts/Runtime/Examples/LayoutMapExample.cs
@@ -40,12 +40,6 @@ namespace MPewsey.ManiaMap.Unity.Examples
             {
                 CreateLayers();
             }
-
-            if (Camera.main != null)
-            {
-                var move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-                Camera.main.transform.Translate(10 * Time.deltaTime * move.normalized);
-            }
         }
 
         /// <summary>
