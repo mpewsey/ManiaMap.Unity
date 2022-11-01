2d342e72f10b0d58bd34a46ca33c6015ed94f183
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-11-01 11:51:21 -0400

    Update Assets.cs

--- Scripts/Tests/PlayMode/Assets.cs
+++ Scripts/Tests/PlayMode/Assets.cs
@@ -21,20 +21,31 @@ namespace MPewsey.ManiaMap.Unity.Tests
         // Rooms
         public const string Angle3x4RoomPath = PackagePath + "Rooms/Angle3x4Room";
 
+        /// <summary>
+        /// Destroys all active GameObjects.
+        /// </summary>
         public static void DestroyAllGameObjects()
         {
-            foreach (var obj in Object.FindObjectsOfType<GameObject>())
+            foreach (var obj in Object.FindObjectsOfType<GameObject>(true))
             {
                 if (obj != null && obj.transform.parent == null)
                     Object.DestroyImmediate(obj);
             }
         }
 
+        /// <summary>
+        /// Loads the prefab from the specified resources path.
+        /// </summary>
+        /// <param name="path">The resources path.</param>
         public static GameObject LoadPrefab(string path)
         {
             return Resources.Load<GameObject>(path);
         }
 
+        /// <summary>
+        /// Instantiates the prefab from the specified resources path.
+        /// </summary>
+        /// <param name="path">The resources path.</param>
         public static T InstantiatePrefab<T>(string path) where T : MonoBehaviour
         {
             var prefab = LoadPrefab(path);
