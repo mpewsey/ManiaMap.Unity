2c8c813387bc1bf28ed442c40f319b74bc7addc4
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2022-11-04 12:11:19 -0400

    Add docs

--- Scripts/Editor/ManiaMapSettingsEditor.cs
+++ Scripts/Editor/ManiaMapSettingsEditor.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 namespace MPewsey.ManiaMap.Unity.Editor
 {
+    /// <summary>
+    /// The ManiaMapSettings editor.
+    /// </summary>
     [CustomEditor(typeof(ManiaMapSettings))]
     public class ManiaMapSettingsEditor : UnityEditor.Editor
     {
+        /// <summary>
+        /// If a ManiaMap/ManiaMapSettings resource does not already exist, creates it.
+        /// </summary>
         [MenuItem("Mania Map/Create Mania Map Settings", priority = 100000)]
         public static void CreateManiaMapSettings()
         {
