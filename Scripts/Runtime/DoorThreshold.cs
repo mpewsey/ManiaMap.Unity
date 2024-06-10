b26096f265187af89cc7a86a9606657c2b225e7b
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-06-10 18:40:22 -0400

    Add AABB to door threshold

--- Scripts/Runtime/DoorThreshold.cs
+++ Scripts/Runtime/DoorThreshold.cs
@@ -7,31 +7,65 @@ namespace MPewsey.ManiaMapUnity
     /// </summary>
     public class DoorThreshold : MonoBehaviour
     {
-        [SerializeField] private Vector3 _center;
-        /// <summary>
-        /// The center of the threshold area.
-        /// </summary>
-        public Vector3 Center { get => _center; set => _center = value; }
-
-        [SerializeField] private Vector3 _size;
+        [SerializeField] private Vector3 _size = Vector3.one;
         /// <summary>
         /// The size of the threshold area.
         /// </summary>
-        public Vector3 Size { get => _size; set => _size = value; }
+        public Vector3 Size { get => _size; set => _size = Vector3.Max(value, Vector3.zero); }
+
+        private void OnValidate()
+        {
+            Size = Size;
+        }
 
         private void OnDrawGizmos()
         {
-            var bounds = AreaBounds();
-            Gizmos.color = Color.yellow;
+            var bounds = GetAABB();
+            var lineColor = Color.yellow;
+            var fillColor = new Color(lineColor.r, lineColor.g, lineColor.b, 0.2f);
+
+            Gizmos.color = fillColor;
+            Gizmos.DrawCube(bounds.center, bounds.size);
+            Gizmos.color = lineColor;
             Gizmos.DrawWireCube(bounds.center, bounds.size);
         }
 
         /// <summary>
-        /// Returns the global bounds of the threshold area.
+        /// Returns the axis aligned bounding box for the threshold area.
         /// </summary>
-        public Bounds AreaBounds()
+        public Bounds GetAABB()
         {
-            return new Bounds(Center + transform.position, Size);
+            var size = 0.5f * Size;
+            var min = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
+            var max = new Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity);
+
+            System.Span<Vector3> corners = stackalloc Vector3[]
+            {
+                new Vector3(-1, -1, -1),
+                new Vector3(1, -1, -1),
+                new Vector3(1, 1, -1),
+                new Vector3(-1, 1, -1),
+                new Vector3(-1, -1, 1),
+                new Vector3(1, -1, 1),
+                new Vector3(1, 1, 1),
+                new Vector3(-1, 1, 1),
+            };
+
+            for (int i = 0; i < corners.Length; i++)
+            {
+                var corner = corners[i];
+                corners[i] = new Vector3(corner.x * size.x, corner.y * size.y, corner.z * size.z);
+            }
+
+            transform.TransformVectors(corners);
+
+            foreach (var corner in corners)
+            {
+                min = Vector3.Min(min, corner);
+                max = Vector3.Max(max, corner);
+            }
+
+            return new Bounds(Vector3.Lerp(min, max, 0.5f) + transform.position, max - min);
         }
 
         /// <summary>
@@ -39,16 +73,15 @@ namespace MPewsey.ManiaMapUnity
         /// The interpolation parameters are clamped to [0, 1].
         /// </summary>
         /// <param name="position">The global position.</param>
-        public Vector3 Parameterize(Vector3 position)
+        public Vector3 ParameterizePosition(Vector3 position)
         {
-            var bounds = AreaBounds();
-            var delta = position - bounds.min;
-            var range = bounds.max - bounds.min;
-
-            var x = range.x == 0 ? 0.5f : Mathf.Clamp01(delta.x / range.x);
-            var y = range.y == 0 ? 0.5f : Mathf.Clamp01(delta.y / range.y);
-            var z = range.z == 0 ? 0.5f : Mathf.Clamp01(delta.z / range.z);
-
+            var bounds = GetAABB();
+            var size = bounds.size;
+            var topLeft = bounds.center - 0.5f * size;
+            var delta = position - topLeft;
+            var x = size.x > 0 ? Mathf.Clamp(delta.x / size.x, 0, 1) : 0.5f;
+            var y = size.y > 0 ? Mathf.Clamp(delta.y / size.y, 0, 1) : 0.5f;
+            var z = size.z > 0 ? Mathf.Clamp(delta.z / size.z, 0, 1) : 0.5f;
             return new Vector3(x, y, z);
         }
 
@@ -57,15 +90,16 @@ namespace MPewsey.ManiaMapUnity
         /// The interpolation parameters are clamped to [0, 1].
         /// </summary>
         /// <param name="parameters">The interpolation parameters.</param>
-        public Vector3 Interpolate(Vector3 parameters)
+        public Vector3 InterpolatePosition(Vector3 parameters)
         {
-            var bounds = AreaBounds();
-            var min = bounds.min;
-            var delta = bounds.max - bounds.min;
+            var bounds = GetAABB();
+            var size = bounds.size;
+            var topLeft = bounds.center - 0.5f * size;
+            var bottomRight = topLeft + size;
 
-            var x = delta.x * Mathf.Clamp01(parameters.x) + min.x;
-            var y = delta.y * Mathf.Clamp01(parameters.y) + min.y;
-            var z = delta.z * Mathf.Clamp01(parameters.z) + min.z;
+            var x = Mathf.Lerp(topLeft.x, bottomRight.x, Mathf.Clamp(parameters.x, 0, 1));
+            var y = Mathf.Lerp(topLeft.y, bottomRight.y, Mathf.Clamp(parameters.y, 0, 1));
+            var z = Mathf.Lerp(topLeft.z, bottomRight.z, Mathf.Clamp(parameters.z, 0, 1));
 
             return new Vector3(x, y, z);
         }
